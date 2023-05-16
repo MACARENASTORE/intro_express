00f37cfddbf4c277abf1f8a711d419676cca064b
MACARENASTORE <oclusiva9204@gmail.com>
2023-05-16 14:45:38 -0500

    v 0.0.2.0

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import express,{ Application, Request, Response, NextFunction } from 'express'
-
+import passport from 'passport'
 import dotenv from 'dotenv'
 
 //llamar el dontenv
@@ -7,6 +7,7 @@ dotenv.config()
 
 import rutas_ejemplo from './routes/rutas_ejemplo'
 import rutas_auth from './routes/rutas_auth'
+import miEstrategia from './config/passports'
 
 //1.Programación funcional
 
@@ -15,7 +16,12 @@ const app:Application =express()
 //1.1 Emplementar las rutas 
 app.use('/auth', rutas_auth)
 
-app.use('/', rutas_ejemplo) 
+//Poteger las siguiente rutas
+
+passport.use(miEstrategia)
+app.use(passport.initialize())
+
+app.use('/', passport.authenticate('jwt', {session:false}), rutas_ejemplo) 
 
 //2. Respuesta cuando el recurso no existe
 
